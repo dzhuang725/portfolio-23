afb8b5697acb83a0032dc4de6631b7729442d73a
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-12-22 23:08:43 -0500

    Edited About section
    - changed layout

--- components/About.tsx
+++ components/About.tsx
@@ -9,42 +9,52 @@ function About({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex flex-col relative h-screen text-center md:text-left xl:flex-row 
-    max-w-7xl px-10 justify-evenly items-center"
+      className="flex flex-col relative h-screen text-center md:text-left
+    max-w-7xl px-10 justify-evenly items-center mx-auto"
     >
       <h3 className="absoute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
-      <motion.img
-        initial={{
-          x: -200,
-          opacity: 0,
-        }}
-        transition={{
-          duration: 1.2,
-        }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        src="https://i.imgur.com/9C5cUow.jpg"
-        className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
-        md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
-      />
+      <div className="flex flex-col lg:flex-row items-center ">
+        <motion.img
+          initial={{
+            x: -200,
+            opacity: 0,
+          }}
+          transition={{
+            duration: 1.2,
+          }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          src="https://i.imgur.com/9C5cUow.jpg"
+          className="mb-20 lg:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+        lg:rounded-3xl lg:w-[500px] lg:h-[600px]"
+        />
 
-      <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl justify-center font-semibold">
-          Here is a{" "}
-          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
-          background
-        </h4>
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div className="space-y-10 px-0 md:px-10">
+          <h4 className="text-4xl justify-center font-semibold">
+            Here is a{" "}
+            <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+            background
+          </h4>
+          <p className="text-base xl:text-lg">
+            I am Weiqi (Dennis) Zhuang, a passionate Software Engineer with over
+            2 years of experience in developing innovative web and iOS
+            applications. My expertise lies in creating stable, scalable, and
+            accessible software products across various platforms. Skilled in
+            languages like TypeScript, JavaScript, C++, Java, and Python, and
+            proficient in frameworks like React, Node.js, and AWS, I have a
+            track record of delivering efficient solutions in software
+            development. My experience includes roles at BeaconFire, BoardX Inc,
+            and DTEN, where I have successfully led and contributed to numerous
+            projects, enhancing user experiences and optimizing software
+            performance. I hold a B.S. in Computer Science and Brain &
+            Behavioral Science from Purdue University, underlining my commitment
+            to continuous learning and professional development. My journey is
+            driven by a user-focused and empathetic approach, aiming to make a
+            meaningful impact through technology.
+          </p>
+        </div>
       </div>
     </motion.div>
   );
